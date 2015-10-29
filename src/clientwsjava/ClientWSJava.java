179e9aba3d326f6c382d2e4345ea5f9a57f304a2
Rio <Rio@Rio-PC>
2015-10-29 23:26:32 +0100

    ClientWSJava funcionant

--- src/clientwsjava/ClientWSJava.java
+++ src/clientwsjava/ClientWSJava.java
@@ -25,29 +25,37 @@ public class ClientWSJava {
     private static int consultaLibres(int idHotel, int fecha) {
         hotel.HotelWS_Service service = new hotel.HotelWS_Service();
         hotel.HotelWS port = service.getHotelWSPort();
-        System.out.println("Hi ha "+port.consultaLibres(idHotel, fecha)+" lliures");
-        return port.consultaLibres(idHotel, fecha);
+        System.out.println("Consulta de l'hotel 1.6102015");
+        int lliures = port.consultaLibres(idHotel, fecha);
+        System.out.println("Hi ha "+lliures+" lliures");
+        return lliures;
     }
 
     private static int reservaHabitacion(int idHotel, int fecha) {
         hotel.HotelWS_Service service = new hotel.HotelWS_Service();
         hotel.HotelWS port = service.getHotelWSPort();
-        System.out.println("S'ha reservat l'habitacio, ara queden "+port.reservaHabitacion(idHotel, fecha)+" lliures");
-        return port.reservaHabitacion(idHotel, fecha);
+        System.out.println("Reserva de l'hotel 1.6102015");
+        int ocupades = port.reservaHabitacion(idHotel, fecha);
+        System.out.println("S'ha reservat l'habitacio, ara n'hi ha "+ocupades+" ocupades");
+        return ocupades;
     }
 
     private static int consultaLibres_1(int idVuelo, int fecha) {
         vuelo.VueloWS_Service service = new vuelo.VueloWS_Service();
         vuelo.VueloWS port = service.getVueloWSPort();
-        System.out.println("Hi ha "+port.consultaLibres(idVuelo, fecha)+ " lliures");
-        return port.consultaLibres(idVuelo, fecha);
+        System.out.println("Consulta del vol 2.7102015");
+        int lliures = port.consultaLibres(idVuelo, fecha);
+        System.out.println("Hi ha "+lliures+ " lliures");
+        return lliures;
     }
 
     private static int reservaPlaza(int idVuelo, int fecha) {
         vuelo.VueloWS_Service service = new vuelo.VueloWS_Service();
         vuelo.VueloWS port = service.getVueloWSPort();
-        System.out.println("S'ha reservat el vol, ara queden "+port.reservaPlaza(idVuelo, fecha)+ " lliures");
-        return port.reservaPlaza(idVuelo, fecha);
+        System.out.println("Reserva del vol 2.7102015");
+        int ocupades = port.reservaPlaza(idVuelo, fecha);
+        System.out.println("S'ha reservat el vol, ara n'hi ha "+ocupades+ " ocupades");
+        return ocupades;
     }
     
 }
